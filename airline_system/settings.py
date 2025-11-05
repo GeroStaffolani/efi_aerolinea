572420350dc9d17ab8da1620d077ff29a1eb1126
GeroStaffolani <g.staffolani@itecriocuarto.org.ar>
2025-11-05 19:06:26 -0300

    cambio tiempo de actualizacion de token

--- airline_system/settings.py
+++ airline_system/settings.py
@@ -52,10 +52,10 @@ REST_FRAMEWORK = {
     ],
 }
 
-# Configuración JWT (puedes personalizar los tiempos si lo deseas)
+# Configuración JWT 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
